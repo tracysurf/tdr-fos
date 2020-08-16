c5c8f9ee3f0b1861c6b333db2b04960f1d974fa6
Mike Ferrara <mferrara@gmail.com>
2020-08-16 04:01:04 -0500

    /albums/{order_id}/rolls endpoint

--- routes/api.php
+++ routes/api.php
@@ -72,20 +72,94 @@ Route::middleware('auth:sanctum')->group(function (){
 
     Route::get('/albums/{order_id}/rolls', function(Request $request, $order_id) {
 
+        $return_array = [
+            'message'   => '',
+            'success'   => false,
+            'data'      => []
+        ];
+
         $user   = $request->user();
         $order  = App\Order::find($order_id);
 
-        $rolls  = \DB::table('photos')->where('order_id', $order_id)->select('roll')->orderBy('roll', 'asc')->distinct();
-
-        $photos = [];
-        foreach($rolls as $roll)
+        if( $order->customer_id !== $user->ID)
         {
-            $roll_photos = App\Photo::where('order_id', $order_id)->where('roll', $roll)->orderBy('filename', 'asc')->get();
-
-            $photos[$roll] = $roll_photos;
+            return [
+                'message'   => 'Order not found', // Purposefully obtuse here as to not confirm that the order exists
+                'success'   => false,
+            ];
         }
 
+        if( ! $order)
+        {
+            return [
+                'message'   => 'Order not found',
+                'success'   => false
+            ];
+        }
 
+        $photos = \App\Photo::where('order_id', $order_id)->orderBy('roll', 'asc')->get();
+
+        $rolls = [];
+        foreach($photos as $photo)
+        {
+            if( ! isset($rolls[$photo->roll]))
+                $rolls[$photo->roll] = 1;
+        }
+
+        foreach($rolls as $roll_id => $nothing)
+        {
+            $roll_photos = App\Photo::where('order_id', $order_id)->where('roll', $roll_id)->orderBy('filename', 'asc')->get();
+
+            $roll_photos_return = [];
+
+            foreach($roll_photos as $roll_photo)
+            {
+                $image_urls = [
+                    'sq'    => $roll_photo->thumbnailURL('_sq'),
+                    'sm'    => $roll_photo->thumbnailURL('_sm'),
+                    'lg'    => $roll_photo->thumbnailURL('_lg'),
+                    'social'=> $roll_photo->thumbnailURL('_social'),
+                ];
+
+                $roll_photos_return[] = [
+                    'id'            => $roll_photo->id,
+                    'image_urls'    => $image_urls,
+                    'liked'         => $roll_photo->favorite ? true : false
+                ];
+
+                $roll_name = $roll_photo->roll_name;
+            }
+
+            $return_array['data'][] = [
+                'id'    => $roll_id,
+                'name'  => $roll_name,
+                'images'=> $roll_photos_return
+            ];
+        }
+
+        $return_array['success'] = true;
+
+        return $return_array;
+    });
+
+    Route::put('/albums/{order_id}', function(Request $request, $order_id){
+
+        $return_array = [
+            'message'   => '',
+            'success'   => false,
+            'data'      => []
+        ];
+
+        $user   = $request->user();
+        $order  = App\Order::find($order_id);
+
+        if( $order->customer_id !== $user->ID)
+        {
+            return [
+                'message'   => 'Order not found', // Purposefully obtuse here as to not confirm that the order exists
+                'success'   => false,
+            ];
+        }
 
         if( ! $order)
         {
