8ba1d7450398a945f52369d6ff2435a27ff96224
Mike Ferrara <mferrara@gmail.com>
2020-09-16 14:38:36 -0500

    Move API endpoints underneath /v1 route prefix

--- routes/api.php
+++ routes/api.php
@@ -18,45 +18,41 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// V1 API routes
 Route::prefix('v1')->group(function() {
-    Route::get('test', function(){
-        echo 'Route prefix test';
+
+    // Auth required routes
+    Route::middleware('auth:sanctum')->group(function (){
+
+        Route::get('/albums',                               '\App\Http\Controllers\API\V1\OrderController@index');
+        Route::put('/albums/{order_id}',                    '\App\Http\Controllers\API\V1\OrderController@update');
+
+        Route::get('/albums/{order_id}/rolls',              '\App\Http\Controllers\API\V1\RollController@index');
+        Route::put('/albums/{order_id}/rolls/{roll_id}',    '\App\Http\Controllers\API\V1\RollController@update');
+
+        Route::get('/notifications',                        '\App\Http\Controllers\API\V1\NotificationController@index');
+        Route::get('/notifications/unseen',                 '\App\Http\Controllers\API\V1\NotificationController@unseen');
+        Route::put('/notifications',                        '\App\Http\Controllers\API\V1\NotificationController@update');
+
+        Route::get('/downloads',                                    '\App\Http\Controllers\API\V1\DownloadController@index');
+        Route::put('/downloads',                                    '\App\Http\Controllers\API\V1\DownloadController@update');
+        Route::put('/albums/{order_id}/rolls/{roll_id}/download',   '\App\Http\Controllers\API\V1\DownloadController@create');
+
+        Route::get('/profile',                              '\App\Http\Controllers\API\V1\UserController@show');
+        Route::put('/profile',                              '\App\Http\Controllers\API\V1\UserController@update');
+
+        Route::put('/albums/{order_id}/rolls/{roll_id}/images/{photo_id}',          '\App\Http\Controllers\API\V1\PhotoController@update');
+        Route::put('/albums/{order_id}/rolls/{roll_id}/images/{photo_id}/rotate',   '\App\Http\Controllers\API\V1\PhotoController@rotate');
+
+        Route::delete('/albums/{order_id}/rolls/{roll_id}/images',                  '\App\Http\Controllers\API\V1\PhotoController@delete');
+
     });
-});
 
-Route::middleware('auth:sanctum')->group(function (){
+    // Sign in
+    Route::post('/auth/signIn', '\App\Http\Controllers\API\V1\AuthController@signIn');
 
-    Route::get('/albums',                               '\App\Http\Controllers\API\V1\OrderController@index');
-    Route::put('/albums/{order_id}',                    '\App\Http\Controllers\API\V1\OrderController@update');
-
-    Route::get('/albums/{order_id}/rolls',              '\App\Http\Controllers\API\V1\RollController@index');
-    Route::put('/albums/{order_id}/rolls/{roll_id}',    '\App\Http\Controllers\API\V1\RollController@update');
-
-    Route::get('/notifications',                        '\App\Http\Controllers\API\V1\NotificationController@index');
-    Route::get('/notifications/unseen',                 '\App\Http\Controllers\API\V1\NotificationController@unseen');
-    Route::put('/notifications',                        '\App\Http\Controllers\API\V1\NotificationController@update');
-
-    Route::get('/downloads',                                    '\App\Http\Controllers\API\V1\DownloadController@index');
-    Route::put('/downloads',                                    '\App\Http\Controllers\API\V1\DownloadController@update');
-    Route::put('/albums/{order_id}/rolls/{roll_id}/download',   '\App\Http\Controllers\API\V1\DownloadController@create');
-
-    Route::get('/profile',                              '\App\Http\Controllers\API\V1\UserController@show');
-    Route::put('/profile',                              '\App\Http\Controllers\API\V1\UserController@update');
-
-
-    Route::put('/albums/{order_id}/rolls/{roll_id}/images/{photo_id}',          '\App\Http\Controllers\API\V1\PhotoController@update');
-    Route::put('/albums/{order_id}/rolls/{roll_id}/images/{photo_id}/rotate',   '\App\Http\Controllers\API\V1\PhotoController@rotate');
-
-    Route::delete('/albums/{order_id}/rolls/{roll_id}/images',                  '\App\Http\Controllers\API\V1\PhotoController@delete');
-
-});
-
-Route::post('/auth/signIn', '\App\Http\Controllers\API\V1\AuthController@signIn');
-
-Route::middleware('privateapiauth')->group(function(){
-    Route::post('/push-notification', '\App\Http\Controllers\API\V1\PushNotificationController@create');
+    // Local/private API between FOS -> this project
+    Route::middleware('privateapiauth')->group(function(){
+        Route::post('/push-notification', '\App\Http\Controllers\API\V1\PushNotificationController@create');
+    });
 });
